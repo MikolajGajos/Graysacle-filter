15c10998e0b041e57a2dd4bdb62b27513983f31d
MikolajGajos <mikus22500@gmail.com>
2022-10-17 20:28:26 +0200

    dobra teraz

---
+++ ProjektJA/ProjektJA/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\ASM.dll")]
+static extern int GreyScaleASM(int a, int b);
+
+[DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\C.dll")]
+static extern int GreyScaleC(int a, int b);
+
+Console.WriteLine(GreyScaleASM(1, 3));
+Console.WriteLine(GreyScaleC(8, 5));
+
