ebb94975eb6e58dc821ba3081bfdd0e0bc0790fd
MikolajGajos <mikus22500@gmail.com>
2022-10-22 16:54:22 +0200

    xd

--- ProjektJA/ProjektJA/Program.cs
+++ ProjektJA/ProjektJA/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System.Runtime.InteropServices;
+using System.Drawing;
 
-[DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\ASM.dll")]
-static extern int GreyScaleASM(int a, int b);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        [DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\ASM.dll")]
+        static extern int GreyScaleASM(int a, int b);
 
-[DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\C.dll")]
-static extern int GreyScaleC(int a, int b);
+        [DllImport(@"D:\studia\github\JAproj\ProjektJA\x64\Debug\C.dll")]
+        static extern int GreyScaleC(int a, int b);
 
-Console.WriteLine(GreyScaleASM(1, 3));
-Console.WriteLine(GreyScaleC(8, 5));
+        Console.WriteLine(GreyScaleASM(1, 3));
+        Console.WriteLine(GreyScaleC(8, 5));
 
+        //Image bitmap = (Bitmap)Image.FromFile(@"mypath.bmp");
+        
+    }
+}
+
+//wczytywanie danych i tworzenie watkow na za tydzien
