a5a9c65d716e93d459799322321bed997198134f
MikolajGajos <mikus22500@gmail.com>
2022-10-23 09:24:58 +0200

    Revert "add threads"
    This reverts commit 4b1b4178d63001e5134eb2c8ecf1d4ae831024a0.

--- ProjektJA/ProjektJA1/Program.cs
+++ ProjektJA/ProjektJA1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Drawing;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace ProjektJA
 {
@@ -19,8 +17,14 @@ namespace ProjektJA
             Console.WriteLine(GreyScaleASM(1, 2));
             Console.WriteLine(GreyScaleC(22, 2));
 
-            GreyScaleManager test = new GreyScaleManager();
-            test.Test(2);
+            Bitmap bmp = new Bitmap(@"D:\dupa\eo.bmp");
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                bmp.SetPixel(i, 0, Color.Blue);
+                bmp.SetPixel(i, 1, Color.Red);
+            }
+            bmp.Save(@"D:\dupa\xd.bmp");
+
 
             Console.ReadLine();
         }
